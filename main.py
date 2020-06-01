e25ca42dacf04c49dab0372f149820545c7da52b
tonywang <tonywang@yuda.tyc.edu.tw>
2020-06-01 14:28:32 +0800

    canvas

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-        from browser import document
+from browser import document, html
+import math
 
-        document <= "Hello2 !"
+canvas = document["zone8"]
+ctx = canvas.getContext("2d")
+
+x = 20
+
+def draw(event):
+    global x
+    ctx.beginPath()
+    ctx.arc(x, 25, 15, 0, 2 * math.pi)
+    x += 15
+    ctx.stroke()
+
+document["button8"].bind("click", draw)
