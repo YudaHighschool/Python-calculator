e227ab0bac437c90faab6dd50216d0352195323c
tonywang <tonywang@yuda.tyc.edu.tw>
2020-06-01 14:32:21 +0800

    calc

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 from browser import document, html
-import math
 
-canvas = document["zone8"]
-ctx = canvas.getContext("2d")
+# Construction de la calculatrice
+calc = html.TABLE()
+calc <= html.TR(html.TH(html.DIV("0", id="result"), colspan=3) +
+                html.TD("C"))
+lines = ["789/", "456*", "123-", "0.=+"]
 
-x = 20
+calc <= (html.TR(html.TD(x) for x in line) for line in lines)
 
-def draw(event):
-    global x
-    ctx.beginPath()
-    ctx.arc(x, 25, 15, 0, 2 * math.pi)
-    x += 15
-    ctx.stroke()
+document <= calc
 
-document["button8"].bind("click", draw)
+result = document["result"] # direct acces to an element by its id
+
+def action(event):
+    """Handles the "click" event on a button of the calculator."""
+    # The element the user clicked on is the attribute "target" of the
+    # event object
+    element = event.target
+    # The text printed on the button is the element's "text" attribute
+    value = element.text
+    if value not in "=C":
+        # update the result zone
+        if result.text in ["0", "error"]:
+            result.text = value
+        else:
+            result.text = result.text + value
+    elif value == "C":
+        # reset
+        result.text = "0"
+    elif value == "=":
+        # execute the formula in result zone
+        try:
+            result.text = eval(result.text)
+        except:
+            result.text = "error"
+
+# Associate function action() to the event "click" on all buttons
+for button in document.select("td"):
+    button.bind("click", action)
